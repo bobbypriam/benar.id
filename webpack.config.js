eec3504160d7709f0009290dab06663d5d610001
Bobby Priambodo <bobby.priambodo@gmail.com>
2016-07-22 10:31:00 +0700

    Mockup barebones

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/entry.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      }
+    ]
+  }
+};
