978f6f34f87892733a428708d12eb0315031a2a2
Bobby Priambodo <bobby.priambodo@gmail.com>
2016-07-23 17:25:31 +0700

    Extract breadcrumbs to partial

--- routes.js
+++ routes.js
@@ -4,26 +4,54 @@ module.exports = [
   {
     path: '/',
     view: 'index',
-    context: { title: 'NewsReview' },
+    context: {
+      title: 'NewsReview',
+    },
   },
   {
     path: '/artikel',
     view: 'article/list',
-    context: { title: 'Artikel | NewsReview' },
+    context: {
+      title: 'Artikel | NewsReview',
+      breadcrumbs: [
+        { path: '/', title: 'Beranda' },
+        { path: '/artikel', title: 'Artikel', active: true },
+      ],
+    },
   },
   {
     path: '/artikel/{id}',
     view: 'article/detail',
-    context: { title: 'Judul Artikel | NewsReview' },
+    context: {
+      title: 'Judul Artikel | NewsReview',
+      breadcrumbs: [
+        { path: '/', title: 'Beranda' },
+        { path: '/artikel', title: 'Artikel' },
+        { path: '/artikel/1', title: 'Judul Artikel', active: true },
+      ],
+    },
   },
   {
     path: '/portal',
     view: 'portal/list',
-    context: { title: 'Portal Berita | NewsReview' },
+    context: {
+      title: 'Portal Berita | NewsReview',
+      breadcrumbs: [
+        { path: '/', title: 'Beranda' },
+        { path: '/portal', title: 'Portal Berita', active: true },
+      ],
+    },
   },
   {
     path: '/portal/{id}',
     view: 'portal/detail',
-    context: { title: 'Portal Berita | NewsReview' },
+    context: {
+      title: 'Portal Berita | NewsReview',
+      breadcrumbs: [
+        { path: '/', title: 'Beranda' },
+        { path: '/portal', title: 'Portal Berita' },
+        { path: '/portal/1', title: 'Detik.com', active: true },
+      ],
+    },
   },
 ];
