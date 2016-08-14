70f25c40ec126a7a2ae999548937f1d455668282
Bobby Priambodo <bobby.priambodo@gmail.com>
2016-08-14 16:42:46 +0700

    Add members fixture

--- data/dao/__tests__/Member.spec.js
+++ data/dao/__tests__/Member.spec.js
@@ -1,12 +1,11 @@
 require('./helpers/common').chai.should()
 
 const Member = require('../Member')
+const members = require('./fixtures/members')
 
 describe('#create()', () => {
   it('should create a new member if called with valid data', () => {
-    const memberData = {
-      email: '',
-    }
+    const memberData = members[0]
     const promise = Member.create(memberData)
     return promise.should.be.fulfilled
   })
---
+++ data/dao/__tests__/fixtures/members.js
@@ -0,0 +1,13 @@
+module.exports = [{
+  email: 'john@email.com',
+  name: 'John Smith',
+  name_slug: 'John-Smith',
+}, {
+  email: 'jane@email.com',
+  name: 'Jane Smith',
+  name_slug: 'Jane-Smith',
+}, {
+  email: 'joe@email.com',
+  name: 'Joe Smith',
+  name_slug: 'Joe-Smith',
+}]
