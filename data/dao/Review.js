c10427dcae5bf2c702690c3bbd19c9c5d671d688
Bobby Priambodo <bobby.priambodo@gmail.com>
2016-08-18 20:48:58 +0700

    Remove Review.create in favor of Article.createReview

--- data/dao/Review.js
+++ data/dao/Review.js
@@ -1,9 +1,5 @@
 const Review = require('../schema/Review')
 
-module.exports.create = function create(reviewData) {
-  return Review.query().insert(reviewData)
-}
-
 // CAUTION: DON'T USE THIS ON APP CODE
 // Helper method for clearing database on tests
 module.exports.clear = function clear() {
--- data/dao/__tests__/Review.spec.js
+++ data/dao/__tests__/Review.spec.js
@@ -27,39 +27,6 @@ before(() =>
   })
 )
 
-describe('#create()', () => {
-  it('should create a new review if called with valid data', () => {
-    const reviewData = Object.assign({}, reviews.valid[0], {
-      article_id: articleId,
-      member_id: memberId,
-    })
-    const promise = Review.create(reviewData)
-    return promise.should.be.fulfilled
-  })
-  it('should reject if article id is not provided', () => {
-    const reviewData = Object.assign({}, reviews.valid[0], {
-      member_id: memberId,
-    })
-    const promise = Review.create(reviewData)
-    return promise.should.be.rejected
-  })
-  it('should reject if member id is not provided', () => {
-    const reviewData = Object.assign({}, reviews.valid[0], {
-      article_id: articleId,
-    })
-    const promise = Review.create(reviewData)
-    return promise.should.be.rejected
-  })
-  it('should reject if content is not provided', () => {
-    const reviewData = Object.assign({}, reviews.invalid.noTitle, {
-      article_id: articleId,
-      member_id: memberId,
-    })
-    const promise = Review.create(reviewData)
-    return promise.should.be.rejected
-  })
-})
-
 afterEach(() => Review.clear())
 
 after(() =>
