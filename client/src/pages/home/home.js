4ec63cc7d1f8b51c135535d2b4efb68c17932eee
Bobby Priambodo <bobby.priambodo@gmail.com>
2016-08-21 16:44:32 +0700

    Got Home.elm working with server provided articles

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,4 +1,9 @@
 const Elm = require('./Home.elm')
 
+/* global data */
+const articles = window.data.articles || []
+
 const node = document.getElementById('home')
-Elm.Home.embed(node)
+const app = Elm.Home.embed(node)
+
+app.ports.articles.send(articles)
