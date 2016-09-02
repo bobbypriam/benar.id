29a2f1c8b29fbbbd0f454bc8204d0e8c0fa53940
Bobby Priambodo <bobby.priambodo@gmail.com>
2016-09-02 20:26:37 +0700

    Hide review form if user has reviewed

--- server/handlers/getArticle.js
+++ server/handlers/getArticle.js
@@ -12,6 +12,14 @@ module.exports = (request, reply) => {
   return Article.get(articleId)
     .then(article => {
       context.article = article
+
+      if (request.auth.isAuthenticated) {
+        context.user.reviewed = article.reviews
+          .filter(review =>
+            review.member.id === context.user.id
+          ).length > 0
+      }
+
       return reply.view('pages/article/detail', context)
     })
     .catch(() => reply('Not found.'))
